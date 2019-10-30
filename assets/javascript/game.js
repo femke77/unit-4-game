12d79510053be9fb93434bffb0789b0774066fb9
Meg Meyers <megan.g.meyers@gmail.com>
2019-10-29 19:17:11 -0700

    basic game setup done, ready for logic

---
+++ assets/javascript/game.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+    
+
+    function getRandom(low, high){
+        var random = Math.floor(Math.random() * high) + low;
+        return random;
+    }
+    
+
+});
